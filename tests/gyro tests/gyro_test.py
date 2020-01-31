8579cb2d12883fa3a05b5d745e0700d51a59f704
Frankie's Raspberry Pi <q24xboss@gmail.com>
2020-01-30 20:33:52 -0500

    Fixed example code for gyro sensor (test more tho)

---
+++ tests/gyro tests/gyro_test.py
@@ -0,0 +1,23 @@
+# MPU6050 9-DoF Example Printout
+
+# https://makersportal.com/blog/2019/11/11/raspberry-pi-python-accelerometer-gyroscope-magnetometer#interfacing=
+
+from mpu9250_i2c import *
+
+time.sleep(1) # delay necessary to allow mpu9250 to settle
+
+print('recording data')
+while 1:
+    try:
+        ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+        # mx,my,mz = AK8963_conv() # read and convert AK8963 magnetometer data
+
+        print('{}'.format('-'*30))
+        print('accel [g]: x = {}, y = {}, z = {}'.format(ax,ay,az))
+        print('gyro [dps]:  x = {}, y = {}, z = {}'.format(wx,wy,wz))
+        # print('mag [uT]:   x = , y = , z = '.format(mx,my,mz))
+        # print('{}'.format('-'*30))
+    except Exception as e:
+        print(str(e))
+        
+    time.sleep(1)
