6b6be6bae5914288deb6798cf895c94c82639e30
neilchattopadhyay123 <56359809+neilchattopadhyay123@users.noreply.github.com>
2021-05-23 21:10:36 -0400

    fix list issues

--- pi_client.py
+++ pi_client.py
@@ -50,21 +50,21 @@ def main():
         # Recieve data
         recv_data = recv(s)
 
-        print(recv_data[1][1])
+        print(recv_data[1])
 
         # Check if a command was sent
         if recv_data[DATA_IDX_COMMAND] == COMMAND_QUIT:  # If quit command was recieved RUNNING = false
             PRINT('Recieved command ' + ENC_VALUE(COMMAND_QUIT) + '.', INFO)
 
             RUNNING = False
-        elif time.time() - last_serial_time >= 1:
-            if recv_data: # checks if recv data is empty
-                print(recv_data)
-                joy_vrt = round(4 * (1 - recv_data[1][3]))
-                joy_fwd = round(4 * (1 - recv_data[1][1]))
-                joy_rot = round(4 * (1 + recv_data[1][2]))
-
-            last_serial_time = time.time()
+        # elif time.time() - last_serial_time >= 1:
+        #     if recv_data: # checks if recv data is empty
+        #         print(recv_data)
+        #         joy_vrt = round(4 * (1 - recv_data[1][3]))
+        #         joy_fwd = round(4 * (1 - recv_data[1][1]))
+        #         joy_rot = round(4 * (1 + recv_data[1][2]))
+        #
+        #     last_serial_time = time.time()
 
     s.close()  # Closes socket
     stream.stop()  # Stops stream
